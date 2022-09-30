e5a716d6be090e581f3b361b1ad58c1c4313f7af
mauridevcde <mauridevcde@gmail.com>
2022-09-30 16:55:15 -0400

    ejemplo de precauciones del uso de useEffect

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,21 +1,27 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 export const Message = () => {
-    
+    const [cordenadas, setCordenadas] = useState({x:0, y:0})
 
     useEffect(() => {
-        //primero
-        console.log("mensaje montado");
-        return () => {
-            //segundo
-            console.log("mensaje desmontado");
+        const onMouseMove = ({x ,y}) => {
+            setCordenadas({x, y})
         }
 
-    },
-    []);
+        window.addEventListener("mousemove", onMouseMove)
+
+        return () => {
+            window.removeEventListener("mousemove", onMouseMove)
+        }
+
+    }, []);
 
   return (
-    <h3 className="mt-2">Usuario ya Existe</h3>
+    <>
+        <h3 className="mt-2">Usuario ya Existe</h3>
+        <h1> {JSON.stringify(cordenadas)} </h1>
+    </>
+    
   )
 }
