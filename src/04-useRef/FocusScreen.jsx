eb902c76a2ccf7340ee95532112bf43f87ff4540
mauridevcde <mauridevcde@gmail.com>
2022-10-17 15:31:55 -0300

    useRef

---
+++ src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+
+export const FocusScreen = () => {
+  const inputRef = useRef();
+  console.log(inputRef);
+ 
+  const handleClick = () => {
+    inputRef.current.select();
+  };
+  return (
+    <>
+    
+      <h1>Focus Screen</h1>
+      
+      <hr />
+
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder="Su nombre"
+        className="form-control"
+      />
+
+      <button className="btn btn-outline-primary mt-2"  onClick={handleClick}> set focus</button>
+
+    </>
+  );
+};
