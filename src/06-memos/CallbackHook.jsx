bfc353041d68b5b75b9af925d85fee4fbe801c51
mauridevcde <mauridevcde@gmail.com>
2022-10-18 15:48:31 -0300

    useCallback

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
-    const { counter, increment } = useState(10);
-  
-    return (
+  const [counter, setCounter] = useState(10);
+  // use calback funciona como useMemo, pero en vez de retornar un valor, retorna una función
+
+  const incrementFather = useCallback(
+      () => {
+        setCounter( (value ) => value + 1);
+      },
+      [],
+    )
+    
+  return (
     <>
-        <h1>useCallBack Hook: {counter}</h1>
+      <h1>useCallBack Hook: {counter}</h1>
+
+      <hr />
+
+      <ShowIncrement increment={incrementFather} />
     </>
-  )
-}
+  );
+};
--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
 
-export const ShowIncrement = ({increment}) => {
+export const ShowIncrement = React.memo(({increment}) => {
+    console.log('Me volví a generar :(');
   return (
     <button className='btn btn-primary' onClick={()=>{increment()}}>
         Incrementar
     </button>
   )
-}
+})
