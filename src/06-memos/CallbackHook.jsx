434676b33854951a96d001730c481b239e5f5134
mauridevcde <mauridevcde@gmail.com>
2022-10-19 14:51:41 -0300

    tarea useCallback

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -5,11 +5,21 @@ export const CallbackHook = () => {
   const [counter, setCounter] = useState(10);
   // use calback funciona como useMemo, pero en vez de retornar un valor, retorna una función
 
+  /*
+  useCallback sin argumentos.
   const incrementFather = useCallback(
       () => {
         setCounter( (value ) => value + 1);
       },
       [],
+    )*/
+
+    // useCallback con argumentos.
+    const incrementFather = useCallback(
+      (valorDeArgumento) => {
+        setCounter( ( element ) => element + valorDeArgumento);
+      },
+      []
     )
     
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,13 @@ import { Layout } from './05-useLayoutEffect/layout'
 import { CallbackHook } from './06-memos/CallbackHook'
 import MemoHook from './06-memos/MemoHook'
 import Memorize from './06-memos/Memorize'
+import { Padre } from './07-tareaMemo/Padre'
 import {HookApp} from './HookApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <CallbackHook />
+    <Padre />
  
   //</React.StrictMode>
 )
