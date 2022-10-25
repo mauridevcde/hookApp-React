06974d12a433520cad6e6510f8e9b4334e2530a6
mauridevcde <mauridevcde@gmail.com>
2022-10-25 16:55:08 -0300

    ReactRouter v6

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,24 @@
+import { Navigate, Route, Routes, Link } from "react-router-dom";
+import { AboutPage } from "./AboutPage";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { NavBar } from "./NavBar";
 
 export const MainApp = () => {
   return (
     <>
-        <h1>Main App</h1>
-        <hr />
+      <h1>Main App</h1>
+
+      <NavBar />
+      <hr />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/about" element={<AboutPage />} />
+
+        {/* <Route path="*" element={<h1>404 Not Found</h1>} /> */}
+        <Route path="*" element={<Navigate to={"/about"} />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
---
+++ src/09-useContext/NavBar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+export const NavBar = () => {
+  return (
+    <>
+      <Link to="/login">Login</Link>
+      <Link to="/about">About</Link>
+      <Link to="/">Home</Link>
+    
+
+
+    </>
+  );
+};
